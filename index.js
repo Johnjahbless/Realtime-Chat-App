7f4dd2fde9a694a2a063c7b19c147927b9990221
johnjahbless <johnjahbless@gmail.com>
2023-11-08 11:18:34 +0100

    add typedefs, models & resolvers

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const { PubSub, withFilter, GraphQLServer } = require('graphql-yoga');
+const { PubSub } = require('graphql-subscriptions');
+const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
 
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+const pubsub = new PubSub();
+
+const server = new GraphQLServer({ typeDefs, resolvers, context: {pubsub}});
+mongoose.connection.once('open', () => 
+    server.start(() => console.log('Server is running')))
